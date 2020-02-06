b25540ab06ec27d572efa22844ee34bdae9be47e
lakkot <40659615+lakkot@users.noreply.github.com>
2020-02-06 22:56:25 +0100

    fiddling with new wf

--- js/wf-new.js
+++ js/wf-new.js
@@ -1,9 +1,18 @@
-var approvers = [
+
+
+
+var approvers = (function() {
+
+var approversRepository = [
   {name: 'Group 1', appr1: 'lakkot83@gmail.com', appr2: 'michal.lichota@outlook.com', appr3: 'michal.lichota@hotmail.com', appr4: '', appr5: ''},
   {name: 'Group 2', appr1: 'michal.lichota@outlook.com', appr2: 'lakkot83@gmail.com', appr3: 'michal.lichota@hotmail.com', appr4: '', appr5: ''},
   {name: 'Group 3', appr1: 'michal.lichota@hotmail.com', appr2: 'michal.lichota@outlook.com', appr3: 'nowaera@gmail.com', appr4: '', appr5: ''},
   {name: 'Group 4', appr1: 'nowaera@gmail.com', appr2: 'michal.lichota@outlook.com', appr3: 'lakkot83@gmail.com', appr4: '', appr5: ''}
 ]
+
+function getAll() {
+return approversRepository;
+}
 
 var $approverGroup = document.querySelector('#wf-ag-group');
 
@@ -17,49 +26,66 @@ function placeHolder(item) {
   $approverGroup.appendChild($placeHolder);
 }
 
+//create option for <select>
 function approverGroups(item) {
   var $approverGroupName = document.createElement('option');
   $approverGroupName.innerText = item.name;
   $approverGroup.appendChild($approverGroupName);
 }
 
-clearList();
-placeHolder();
-approvers.forEach(function(item) {
-  approverGroups(item);
-})
-//hide line if no approver field is empty
+
+//fill in table with approvers based on group chosen in <select>
 $approverGroup.addEventListener('change', function(item) {
-
-
-
   if ($approverGroup.value !==   "...") {
     $('.wf-approver').removeClass('d-none');
-    index = approvers.findIndex(item => item.name === $approverGroup.value);
-    document.querySelector('#wf-approver1').innerText = approvers[index].appr1;
-    if (approvers[index].appr2 !== '') {
-      document.querySelector('#wf-approver2').innerText = approvers[index].appr2;
+    //find object in array based on .name value
+    //get index of object with corresponding name
+    index = approversRepository.findIndex(item => item.name === $approverGroup.value);
+    document.querySelector('#wf-approver1').innerText = approversRepository[index].appr1;
+    //if approver field is not empty, fill in with corresponding approver. if it is, hide it
+    if (approversRepository[index].appr2 !== '') {
+      document.querySelector('#wf-approver2').innerText = approversRepository[index].appr2;
     } else {
+      //hide line if approver field is empty
       document.querySelector('#wf-approver2__row').classList.add('d-none');
     }
-    if (approvers[index].appr3 !== '') {
-      document.querySelector('#wf-approver3').innerText = approvers[index].appr3;
+    if (approversRepository[index].appr3 !== '') {
+      document.querySelector('#wf-approver3').innerText = approversRepository[index].appr3;
     } else {
       document.querySelector('#wf-approver3__row').classList.add('d-none');
     }
-    if (approvers[index].appr4 !== '') {
-      document.querySelector('#wf-approver4').innerText = approvers[index].appr4;
+    if (approversRepository[index].appr4 !== '') {
+      document.querySelector('#wf-approver4').innerText = approversRepository[index].appr4;
     } else {
       document.querySelector('#wf-approver4__row').classList.add('d-none');
     }
-    if (approvers[index].appr5 !== '') {
-      document.querySelector('#wf-approver5').innerText = approvers[index].appr5;
+    if (approversRepository[index].appr5 !== '') {
+      document.querySelector('#wf-approver5').innerText = approversRepository[index].appr5;
     } else {
       document.querySelector('#wf-approver5__row').classList.add('d-none');
     }
   } else {
     $('.wf-approver').addClass('d-none');
   }
+})
+return {
+  getAll: getAll,
+  clearList: clearList,
+  placeHolder: placeHolder,
+  approverGroups: approverGroups
+
+}
+
+}());
 
 
+
+
+
+
+//clear approvers list, insert placeholder and name for each object form approvers array
+approvers.clearList();
+approvers.placeHolder();
+approvers.getAll().forEach(function(item) {
+  approvers.approverGroups(item);
 })
