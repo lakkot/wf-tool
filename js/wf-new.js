8b3e0dff843873e081c0321cd7bfa6feeda4072b
lakkot <40659615+lakkot@users.noreply.github.com>
2020-02-06 19:42:30 +0100

    fixing issue where functions didnt work if you  chose '...' in approver group on new wf

--- js/wf-new.js
+++ js/wf-new.js
@@ -11,6 +11,11 @@ function clearList() {
   $approverGroup.innerHTML = '';
 }
 
+function placeHolder(item) {
+  var $placeHolder = document.createElement('option');
+  $placeHolder.innerText = "...";
+  $approverGroup.appendChild($placeHolder);
+}
 
 function approverGroups(item) {
   var $approverGroupName = document.createElement('option');
@@ -19,31 +24,42 @@ function approverGroups(item) {
 }
 
 clearList();
+placeHolder();
 approvers.forEach(function(item) {
   approverGroups(item);
 })
-
+//hide line if no approver field is empty
 $approverGroup.addEventListener('change', function(item) {
-  index = approvers.findIndex(item => item.name === $approverGroup.value);
-  document.querySelector('#wf-approver1').innerText = approvers[index].appr1;
-  if (approvers[index].appr2 !== '') {
-    document.querySelector('#wf-approver2').innerText = approvers[index].appr2;
+
+
+
+  if ($approverGroup.value !==   "...") {
+    $('.wf-approver').removeClass('d-none');
+    index = approvers.findIndex(item => item.name === $approverGroup.value);
+    document.querySelector('#wf-approver1').innerText = approvers[index].appr1;
+    if (approvers[index].appr2 !== '') {
+      document.querySelector('#wf-approver2').innerText = approvers[index].appr2;
+    } else {
+      document.querySelector('#wf-approver2__row').classList.add('d-none');
+    }
+    if (approvers[index].appr3 !== '') {
+      document.querySelector('#wf-approver3').innerText = approvers[index].appr3;
+    } else {
+      document.querySelector('#wf-approver3__row').classList.add('d-none');
+    }
+    if (approvers[index].appr4 !== '') {
+      document.querySelector('#wf-approver4').innerText = approvers[index].appr4;
+    } else {
+      document.querySelector('#wf-approver4__row').classList.add('d-none');
+    }
+    if (approvers[index].appr5 !== '') {
+      document.querySelector('#wf-approver5').innerText = approvers[index].appr5;
+    } else {
+      document.querySelector('#wf-approver5__row').classList.add('d-none');
+    }
   } else {
-    document.querySelector('#wf-approver2__row').classList.add('d-none');
+    $('.wf-approver').addClass('d-none');
   }
-  if (approvers[index].appr3 !== '') {
-    document.querySelector('#wf-approver3').innerText = approvers[index].appr3;
-  } else {
-    document.querySelector('#wf-approver3__row').classList.add('d-none');
-  }
-  if (approvers[index].appr4 !== '') {
-    document.querySelector('#wf-approver4').innerText = approvers[index].appr4;
-  } else {
-    document.querySelector('#wf-approver4__row').classList.add('d-none');
-  }
-  if (approvers[index].appr5 !== '') {
-    document.querySelector('#wf-approver5').innerText = approvers[index].appr5;
-  } else {
-    document.querySelector('#wf-approver5__row').classList.add('d-none');
-  }
+
+
 })
